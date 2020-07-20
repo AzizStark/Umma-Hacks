6df116d6c185320c08cd25d3c2db026ee646c644
AzizStark <theazizstark@gmail.com>
2020-07-21 00:24:43 +0530

    Add build pack

---
+++ client/build/precache-manifest.beacdc0771263f59188fbef941aeb1a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c124b74016e3ccb1646773ebc5a9ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d9f51beb1e4825c716",
+    "url": "/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "d1b18c396f60a467d4e0",
+    "url": "/static/css/main.e1d0884a.chunk.css"
+  },
+  {
+    "revision": "d1d9f51beb1e4825c716",
+    "url": "/static/js/2.4b33c427.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4b33c427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b18c396f60a467d4e0",
+    "url": "/static/js/main.254f6cf2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
