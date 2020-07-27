29181db64113716ef59b649a7bbaa276168908c2
Saima Yunus <saimayunus925@gmail.com>
2020-07-26 23:28:02 -0700

    form in calculator/Form.js, not rendering

---
+++ client/src/app/calculator/Form.js
@@ -0,0 +1,101 @@
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css'; 
+
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // minimum value of money to have so you can give zakat
+      nisab: '',
+      // gold and silver values
+      goldValue: '',
+      goldOz: '',
+      silverValue: '',
+      silverOz: '',
+      // cash values
+      cash: '', // cash on hand and in bank accts
+      deposits: '',
+      loan: '',
+      businessInvestments: '',
+      // trade
+      stock: '',
+      // liabilities
+      borrowedMoney: '',
+      wages: '',
+      taxRentUtility: '',
+      // total amount
+      total: ''
+    }
+  }
+
+  formSubmit = () => {
+    const { nisab, goldValue, goldOz, silverValue, silverOz, cash, deposits, loan, businessInvestments, stock, borrowedMoney, wages, taxRentUtility } = this.state;
+    // gold and silver values 
+    let gold = goldOz * goldValue;
+    let silver = silverOz * silverValue;
+    // cash values
+    let totalCash = cash + deposits + businessInvestments + loan;
+    // liabilities 
+    let totalLiabilities = borrowedMoney + wages + taxRentUtility;
+    let total = parseInt(gold) + parseInt(silver) + parseInt(totalCash) + parseInt(stock) - parseInt(totalLiabilities);
+    // this.setState({total: parseInt(total)});
+    if (total >= 6056.85) {
+      document.querySelector("#zakatOutput").innerHTML = `<h6>Total Zakat Due This Year: ${0.025 * total}</h6>`;
+    }
+    else {
+      document.querySelector("#zakatOutput").innerHTML = "<h6>No Zakat Due This Year</h6>"
+    }
+  } 
+  render() {
+    return (
+      <div id = "app">
+            <form>
+              <label>
+                Nisab:
+                <input type="number" name="nisab" placeholder="$6056.85" onChange= {e => this.setState({nisab: e.target.value})}/>
+              </label>
+              <label>
+                Gold Value Today:
+                <input type="number" name="goldValue" placeholder="$2018.95" onChange= {e => this.setState({goldValue: e.target.value})}/>
+              </label>
+              <label>
+                Gold (in oz):
+                <input type="number" name="goldOz" onChange= {e => this.setState({goldOz: e.target.value})}/>
+              </label>
+              <label>
+                Silver Value Today:
+                <input type="number" name="silverValue" placeholder="$22.89" onChange= {e => this.setState({silverValue: e.target.value})}/>
+              </label>
+              <label>
+                Silver (in oz):
+                <input type="number" name="silverOz" onChange= {e => this.setState({silverOz: e.target.value})}/>
+              </label>
+              <label>
+                Cash:
+                <input type="number" name="cash" onChange= {e => this.setState({cash: e.target.value})} />
+              </label>
+              <label>
+                Loan:
+                <input type="number" name="loan" onChange= {e => this.setState({loan: e.target.value})}/>
+              </label>
+              <label>
+                Stock Value:
+                <input type="number" name="stock" onChange= {e => this.setState({stock: e.target.value})} />
+              </label>
+              <label>
+                Liability:
+                <input type="number" name="liability" onChange= {e => this.setState({liability: e.target.value})}/>
+              </label>
+              <button type="button" onClick = {() => this.formSubmit()}>Submit</button>
+            </form>
+          <div id="zakatOutput">
+            
+          </div> 
+
+        </div>
+    )
+  }
+  
+}
--- client/src/app/calculator/calculator.js
+++ client/src/app/calculator/calculator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Form from './Form.js';
 export default class Calculator extends React.Component {
 
     constructor(props) {
@@ -9,6 +9,22 @@ export default class Calculator extends React.Component {
             amount: '',
             zakat: ''
         }
+        /**
+         * <div className="box" style={{margin: 'auto', marginTop: '8%'}}>
+                            <input className="input is-normal" type="number" placeholder="Enter amount" /*This functn sets the input value into state variable -->  onChange={(e) => this.setState({ amount: e.target.value })} />
+                            <br /> <br />
+                            <input className="button is-block is-info is-normal is-fullwidth" type="button" value="Calculate Zakat" /* Function is triggered here -->  onClick={() => this.calculateZakat()} />
+                            this.state.zakat ? "" :
+                                <div>
+                                    <br />
+                                    <h3> Calculated Zakat: {this.state.zakat} </h3>
+                                    <br />
+                                    <h4> Do you want to donate? </h4> <br />
+                                    <center><input onClick={() => this.props.history.push('/donate')} className="button is-block is-info" type="button" value="Yes" /></center>
+                                </div>
+                            
+                        </div>
+         */
     }
 
     //Function for calculating zakat
@@ -31,22 +47,10 @@ export default class Calculator extends React.Component {
                     <div className="column" style={{ backgroundColor: 'rgb(230, 213, 255)', display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '100vh' }}>
                         <h5 className="title is-5" style={{color: '#3298dc', marginTop: '6%'}}>Are you tired of Zakat guides being archaic and outdated? <br />
                         Are you worried you might not know how to fulfill this pillar of Islam? <br />
-                        Fear not - you've come to the right place! 
+                        Fear notght place! 
                         </h5>
-                        <div className="box" style={{margin: 'auto', marginTop: '8%'}}>
-                            <input className="input is-normal" type="number" placeholder="Enter amount" /*This functn sets the input value into state variable --> */ onChange={(e) => this.setState({ amount: e.target.value })} />
-                            <br /> <br />
-                            <input className="button is-block is-info is-normal is-fullwidth" type="button" value="Calculate Zakat" /* Function is triggered here --> */ onClick={() => this.calculateZakat()} />
-                            {!this.state.zakat ? "" :
-                                <div>
-                                    <br />
-                                    <h3> Calculated Zakat: {this.state.zakat} </h3>
-                                    <br />
-                                    <h4> Do you want to donate? </h4> <br />
-                                    <center><input onClick={() => this.props.history.push('/donate')} className="button is-block is-info" type="button" value="Yes" /></center>
-                                </div>
-                            }
-                        </div>
+               
+                        <Form />
                     </div>
                 </div>
                </div>
